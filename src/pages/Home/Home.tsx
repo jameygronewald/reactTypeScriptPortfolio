96b57364097497dee35d3b4c8949710de2d27679
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-23 13:01:13 -0700

    edited grid on homepage and added icons for MERN techs; modularized grid and BioText into components for reuse

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { BigHeader } from "../../components/BigHeader/BigHeader";
-import { SmallHeader } from "../../components/SmallHeader/SmallHeader";
 import { BioImage } from "../../components/BioImage/BioImage";
 import { BioText } from "../../components/BioText/BioText";
+import { IconGridMERN } from "../../components/IconGrids/IconGridMERN";
 
 export const Home = () => {
   return (
     <div className="homeContainer">
       <BigHeader text="Welcome. My name is Jamey." />
-      <SmallHeader text="Here's some info about me." />
       <BioImage />
-      <BioText />
+      <BioText text="I am a full-stack web developer that primarily works creating MERN stack
+        applications."/>
+      <IconGridMERN />
     </div>
   );
 };
