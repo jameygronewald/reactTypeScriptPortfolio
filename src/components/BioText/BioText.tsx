9a95521c91a9b1a4b2ed6331f38eb38829044e3d
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-22 21:59:03 -0700

    edited navbar to use grid and created initial grid with some content for the home page

---
+++ src/components/BioText/BioText.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const BioText = () => {
+  return (
+    <div className="bioText">
+      Full-stack web developer with skills in JavaScript, React, Express,
+      node.js, and MongoDB. Passionate about learning and discovering new
+      avenues through which to participate in the creative process. Firmly
+      believe that the experience of others should form the basis of products
+      and services. Interested in storytelling and building new platforms from
+      which artists can create and be heard. From Atlanta, GA and based out of
+      Seattle, WA.
+    </div>
+  );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BigHeader } from "../../components/BigHeader/BigHeader";
 import { SmallHeader } from "../../components/SmallHeader/SmallHeader";
+import { BioImage } from "../../components/BioImage/BioImage";
+import { BioText } from "../../components/BioText/BioText";
 
 export const Home = () => {
   return (
-    <>
+    <div className="homeContainer">
       <BigHeader text="Welcome. My name is Jamey." />
       <SmallHeader text="Here's some info about me." />
-    </>
+      <BioImage />
+      <BioText />
+    </div>
   );
 };
