4e637be2da90e0cadf05ffdd0f2db9292f55394f
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-27 12:32:16 -0700

    added material UI inputs into form for better styling

--- client/src/pages/Contact/Contact.tsx
+++ client/src/pages/Contact/Contact.tsx
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
 import { BigHeader } from "../../components/BigHeader/BigHeader";
+// import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//       "& .MuiTextField-root": {
+//         margin: theme.spacing(1),
+//         width: "40%",
+//       },
+//     },
+//   })
+// );
 
 export const Contact = () => {
+  // const classes = useStyles();
+
   const [formData, setFormData] = useState({});
 
   const handleSubmit = (e: any) => {
@@ -15,44 +30,66 @@ export const Contact = () => {
         class="contactHeader"
         text="Let's connect and build something."
       />
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} /* className={classes.root} */>
         <div className="formRow1">
-          <input
-            type="text"
+          <TextField
+          className="input"
+            required
+            label="First Name"
+            variant="outlined"
             name="firstName"
-            placeholder="First Name"
             onChange={e => {
               setFormData({ ...formData, firstName: e.target.value });
             }}
           />
-          <input
-            type="text"
+          <TextField
+          className="input"
+            required
+            label="Last Name"
+            variant="outlined"
             name="lastName"
-            placeholder="Last Name"
             onChange={e => {
               setFormData({ ...formData, lastName: e.target.value });
             }}
           />
         </div>
         <div className="formRow2">
-          <input
-            type="text"
+          <TextField
+          className="input"
+            required
+            label="Email"
+            variant="outlined"
             name="email"
-            placeholder="Email"
             onChange={e => {
               setFormData({ ...formData, email: e.target.value });
             }}
           />
-          <input
-            type="textarea"
+          <TextField
+          className="input"
+            label="Phone Number"
+            variant="outlined"
+            name="email"
+            onChange={e => {
+              setFormData({ ...formData, phone: e.target.value });
+            }}
+          />
+        </div>
+        <div className="formRow3">
+          <TextField
+            label="Enter message here"
+            className="messageInput"
+            multiline
+            rows={4}
+            variant="outlined"
             name="message"
-            placeholder="Enter message here"
             onChange={e => {
               setFormData({ ...formData, message: e.target.value });
             }}
           />
         </div>
-        <input type="submit" value="submit" />
+        <div className="formRow4">
+          <input type="submit" value="submit" />
+        </div>
       </form>
     </div>
   );
